13f807f36507ae25a3d7b28f5385f4024d8c7dd6
Avyaya Kumar <128767177+AvyayaKumar@users.noreply.github.com>
2026-09-02 13:16:55 -0700

    Add: combination-target-sum - submission-4

---
+++ Data Structures & Algorithms/combination-target-sum/submission-4.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int target;
+    vector<int> nums;
+    vector<vector<int>> ans;
+    vector<vector<int>> combinationSum(vector<int>& nums_inp, int target_inp) {
+        nums = nums_inp;
+        target = target_inp;
+        sort(nums.begin(), nums.end());
+        helper(0, 0, {});
+        return ans;
+    }
+    void helper(int i, int curr_sum, vector<int> curr_set) {
+        if (i >= nums.size()) return;
+        if (curr_sum == target) {
+            ans.push_back(curr_set);
+            return;
+        }
+        for (int j = i; j < nums.size(); j++) {
+            curr_sum += nums[j];
+            curr_set.push_back(nums[j]);
+            if (curr_sum <= target) {
+                helper(j, curr_sum, curr_set);
+                curr_sum -= nums[j];
+                curr_set.pop_back();
+            } else {
+                break;
+            }
+        }
+    }
+};
