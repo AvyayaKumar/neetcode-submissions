3a7628068952470742f0b326beb5cf33b011dfa4
Avyaya Kumar <128767177+AvyayaKumar@users.noreply.github.com>
2026-09-11 12:59:02 -0700

    Add: course-schedule-ii - submission-3

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-3.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> ans;
+    vector<vector<int>> adj_list;
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        // unordered_map<int, vector<int>> adj_list;
+        adj_list.resize(numCourses, {});        
+        for (const auto& v : prerequisites) {
+            adj_list[v[0]].push_back(v[1]);
+        }
+        // unordered_set<int> visited;
+        vector<bool> visited(numCourses, 0);
+        unordered_set<int> path;
+        for (int i = 0; i < numCourses; i++) {
+            if (!dfs(i, visited, path, adj_list)) return {};
+        }
+        return ans;
+    }
+    bool dfs(int node, vector<bool>& visited, unordered_set<int>& path, vector<vector<int>>& adj_list) {
+        if (path.contains(node)) return false;
+        if (visited[node]) return true;
+        path.insert(node);
+        visited[node] = true;
+        for (int neighbor : adj_list[node]) {
+            if (!dfs(neighbor, visited, path, adj_list)) return false;
+        }
+        path.erase(node);
+        ans.push_back(node);
+        return true;
+    }
+};
